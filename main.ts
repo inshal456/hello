1ef83844ab9eb97fd9becc8cc41be038cc0e1a07
inshal456 <160832350+inshal456@users.noreply.github.com>
2024-03-06 16:56:49 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,44 @@
+//let num1=(3)
+//let num2=(5)
+//console.log(num1+num2)
+  //let isMarried=true
+  //console.log(isMarried)//
+
+
+
+
+
+
+//UNARY OPERATIONS
+
+// two types od incremint 
+// pre incremnt or post increment 
+
+//1)pre increment
+
+ //let a=0
+// console.log(++a)
+ //console.log(a)
+ //console.log(++a)
+  //console.log(a)
+  //console.log(a++)
+ // console.log(a)
+  //console.log(a++)
+ // console.log(a)
+   
+///2)post
+ //let a=4
+ //console.log(a++) 
+ //console.log(a)
+
+ //let a=4
+ //console.log(++a) 
+ //console.log(a =a-3)
+ 
+
+ //let a=9
+ //console.log(--a)
+
+ let a=10
+ console.log(a--)
+ console.log(a)
